7342a177624a3ae0bd4a5593605f769419b75e5d
Muyideen Khalifa <dixonmk400@gmail.com>
2022-05-14 16:55:07 +0100

    set production db & start command

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const configurations = {
   PORT: process.env.PORT || 4000,
   MONGODB_HOST: process.env.MONGODB_HOST || "localhost",
   MONGODB_DATABASE: process.env.MONGODB_DB || "notes-app",
-  MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || "localhost"}/${
+  MONGODB_URI: process.env.MONGODB_URL || `mongodb://${process.env.MONGODB_HOST || "localhost"}/${
     process.env.MONGODB_DATABASE || "notes-app"
   }`,
 };
